60cf307757b42a84c34847ce364559e3efab5f29
JamesCorrigan <James_Corrigan@iCloud.com>
2019-02-12 15:35:20 -0600

    basic ui outline

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CS3308Project
 Group Project for CSCI 3308
+
+Docs:
+
+  API:
+    https://developers.facebook.com/docs/graph-api/
+
+    https://www.reddit.com/dev/api/
+
+    https://developer.twitter.com/en/docs/twitter-for-websites/overview
+  
---
+++ ui-demo/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+/*
+Keep this small, import components from folders
+*/
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            CSCI 3308 Group Project
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
