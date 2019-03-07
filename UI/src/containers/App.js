a514f8cf8989e10a398cbf08083a5f66c6f72124
JamesCorrigan <James_Corrigan@iCloud.com>
2019-03-06 21:39:58 -0700

    merged style with old form

--- UI/src/containers/App.js
+++ UI/src/containers/App.js
@@ -1,48 +1,66 @@
-import React, {Component} from 'react';
+import React, {Component, Button} from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../components/Home/Home.js';
 import Vacations from '../components/Vacations/Vacations.js';
 import Header from '../components/Header/Header.js';
 import PhotoAlbum from '../components/PhotoAlbum/PhotoAlbum.js';
 import MealPlan from '../components/MealPlan/MealPlan.js';
+import LoginModal from '../components/basic/Modal.js';
+import LoginForm from '../components/basic/LoginForm.js';
+import RegisterForm from '../components/basic/RegisterForm.js';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      loggedIn: false,
+      showModal: false
     }
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  componentDidMount() {
-    //this.callUsers().then(res => )
-    //this.callUsers().then(res => this.setState({data: res}))
-    //.catch(err => console.log(JSON.stringify(err)));
-    //console.log(this.state)
+  handleClose = e => {
+    e.preventDefault();
+    this.setState({showModal: false});
   }
-  //demo server call
-  callBackendAPI = async () => {
-    const res = await fetch('/');
-    const body = await res.json();
-     if (res.status !== 200) {
-       throw Error(body.message)
-     }
-     return body;
-  };
 
-  callUsers = async () => {
-    const response = await fetch('/users');
-    console.log('response', response);
-    const body = await response.json();
-    console.log('body', body);
-     if (response.status !== 200) {
-       throw Error(body.message)
-     }
-     return body;
-  };
+  handleShow = e => {
+    e.preventDefault();
+    this.setState({showModal: true});
+  }
   render() {
+    const loginModal = (
+      <LoginModal
+        handleClose={this.handleClose}
+      >
+      </LoginModal>
+    );
+
+    const header = (
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <a className="navbar-brand" href="familySite.html">FamilyTime</a>
+          <Link to="/" className='header-link'>Home</Link>
+          <Link to="/photoalbum" className='header-link'>Album</Link>
+          <Link to="/vacations" className='header-link'>Vacations</Link>
+          <Link to="/mealplan" className='header-link'>Meals</Link>
+          <ul className="navbar-nav mr-auto">
+            </ul>
+            <div className="form-inline mt-2 mt-md-0">
+              <button className="btn btn-outline-success my-2 my-sm-0" onClick={this.handleShow}>
+                Login
+              </button>
+            </div>
+        </nav>
+      </header>
+    );
+
     return (
       <div>
-        <Header />
+        {header}
+        {this.state.showModal ? loginModal : null}
         <main>
           <Route exact path="/" component={Home} />
           <Route exact path="/vacations" component={Vacations} />
--- UI/src/index.js
+++ UI/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.css';
 //Importing store and history allows use of Router and Redux
 import store, { history } from './store';
 import App from './containers/App.js';
