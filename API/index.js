3405dbe8bb4c2bfbe2d2481f75bb5ccf40b1de7e
JamesCorrigan <James_Corrigan@iCloud.com>
2019-03-07 11:00:55 -0700

    removed files

--- API/index.js
+++ API/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
+//db = all database functions
 const db = require('./queries');
+//required parser for JSON objects
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -10,15 +12,23 @@ app.use(
   })
 );
 
+//allows app to use url routes
 var router = express.Router();
 
+//For root, return simple info
 app.get('/', (req, res) => {
   res.json({ info: 'api info'});
 });
 
+//GET to get all users (old)
 app.get('/users', db.getUsers);
+//GET to get one user (old)
 app.get('/users/:id', db.getUserById);
+
+//PUT to get one user (old)
 app.put('/users/:id', db.updateUser);
+
+//DELETE a user (old)
 app.delete('/users/:id', db.deleteUser);
 app.post('/register', db.register);
 app.post('/login', db.login);
