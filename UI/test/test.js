76c8af52f18f4c5d0e948cbe7c18d00d4ba6518a
KwesiObika <Kwesi.Obika@colorado.edu>
2019-04-27 14:03:02 -0600

    testing

---
+++ UI/test/test.js
@@ -0,0 +1,82 @@
+var assert = require('assert');
+// describe('API', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+// });
+describe('UI', function() {
+  describe('function getAllFamilyImages(familyID)', function() {
+    it('should return an array of all images', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function addImageToDB(url, family)', function() {
+    it('should return title of image', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function getImageHandler(family)', function() {
+    it('should return 0 for correct output', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('recieveImages(obj)', function() {
+    it('should return url of image recieved', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('responseHandler(response)', function() {
+    it('should return 0 for correct output', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+describe('API', function() {
+  describe('function getUsers(req, res)', function() {
+    it('should return array of all user names', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function getUserById(req, res)', function() {
+    it('should return the user name for ID [1]', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function getUserByEmail(email)', function() {
+    it('should return the user name for email[ahoto@gmail.com]', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function addImageToDB(req, res)', function() {
+    it('should return the position of the image added', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('function updateUser(req, res)', function() {
+    it('should return the updated User', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
